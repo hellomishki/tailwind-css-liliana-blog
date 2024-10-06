e90bd3eef85d4d3f69661531a93be44d31af3a22
Liliana Summers <hello@lilianasummers.com>
2024-10-06 14:02:03 +1100

    Revert "Update package.json and layout.tsx"
    This reverts commit ba96e919958b95e075d975f7889c91041da1ff5d.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import 'css/tailwind.css'
 import 'pliny/search/algolia.css'
 import 'remark-github-blockquote-alert/alert.css'
 import { Inria_Sans } from 'next/font/google'
-import { Analytics } from '@vercel/analytics/react'
+import { Analytics, AnalyticsConfig } from 'pliny/analytics'
 import { SearchProvider, SearchConfig } from 'pliny/search'
 import Header from '@/components/Header'
 import SectionContainer from '@/components/SectionContainer'
@@ -97,7 +97,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <link rel="alternate" type="application/rss+xml" href={`${basePath}/feed.xml`} />
       <body className="bg-white pl-[calc(100vw-100%)] text-black antialiased dark:bg-black dark:text-white">
         <ThemeProviders>
-          <Analytics />
+          <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
           <SectionContainer>
             <SearchProvider searchConfig={siteMetadata.search as SearchConfig}>
               <Header />
